d6ee73ec0480d237c9f50f97b291f31aa43ab938
Antonio Luna <antonio__luna@outlook.es>
2015-04-26 21:24:19 +0200

    correcciones

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ response
 import requests
 from requests_oauthlib import OAuth1, OAuth1Session
 
-with open(consumer.key) as com_key:
+with open("consumer.key", "r") as com_key:
     CONSUMER_KEY = com_key.read()
 
-with open(consumer.scrt) as com_scrt:
-    CONSUMER_SECRET = cpm_scrt.read()
+with open("consumer.scrt", "r") as com_scrt:
+    CONSUMER_SECRET = com_scrt.read()
 
 REQUEST_TOKEN_URL = 'https://api.discogs.com/oauth/request_token'
 AUTHENTICATE_URL = 'https://www.discogs.com/oauth/authorize'
