45e0a1a7c8ad8b0c0d5b6b5c3b5f34ccdea94d94
Antonio Luna <antonio__luna@outlook.es>
2015-04-29 12:32:44 +0200

    más métodos

--- setup.py
+++ setup.py
@@ -8,6 +8,23 @@ import requests
 from requests_oauthlib import OAuth1, OAuth1Session
 
 scrobble = requests.get('http://ws.audioscrobbler.com/2.0/?')
+
 metodos = {'album_informacion': 'Album.getInfo',
-'album_gritos': 'Album.getShouts', 'album_etiquetas': 'Album.getTags',
-'album_etiquetas_top': 'Album.getTopTags', 'album_buscar': 'Album.search'}
+'album_comentarios': 'Album.getShouts', 'album_etiquetas': 'Album.getTags',
+'album_etiquetas_top': 'Album.getTopTags', 'album_buscar': 'Album.search',
+
+'artista_corregir': 'Artist.getCorrection',
+'artista_informacion': 'Artist.getInfo', 'artista_eventos': 'Artist.getEvents',
+'artista_similar': 'Artist.getSimilar',
+'artista_mejores_discos': 'Artist.getTopAlbums',
+'artista_mejores_canciones': 'Artist.getTopTracks',
+'artista_': 'Artist.getTags', 'artista_comentarios': 'Artist.getShouts',
+'artista_buscar': 'Artist.search',
+
+'geo_eventos': 'Geo.getEvents', 'geo_chart_artista': 'Geo.getMetroArtistChart',
+'geo_hype_artista_ciudad': 'Geo.getMetroHypeArtistChart',
+'geo_hype_canciones_ciudad': 'Geo.getMetroHypeTrackChart',
+'geo_chart_semanal': 'Geo.getMetroWeeklyChartlist',
+'geo_artistas_ciudad': 'Geo.getTopArtists',
+'geo_canciones_ciudad': 'Geo.getTopTracks',
+'geo_obtener_ciudades': 'Geo.getMetros'}
