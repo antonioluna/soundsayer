2ebfd1971eb448aaf8600fab685079d90d095bcc
Antonio Luna <antonio__luna@outlook.es>
2015-04-29 12:04:44 +0200

    añado métodos de album

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ import requests
 from requests_oauthlib import OAuth1, OAuth1Session
 
 scrobble = requests.get('http://ws.audioscrobbler.com/2.0/?')
-metodos = {}
-
-#hola
+metodos = {'album_informacion': 'Album.getInfo',
+'album_gritos': 'Album.getShouts', 'album_etiquetas': 'Album.getTags',
+'album_etiquetas_top': 'Album.getTopTags', 'album_buscar': 'Album.search'}
