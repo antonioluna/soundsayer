d6ae24e3d271b35c1ee2ed0925ce1a68a993646b
Antonio Luna <antonio__luna@outlook.es>
2015-04-29 11:57:33 +0200

    modificacion

--- setup.py
+++ setup.py
@@ -6,3 +6,8 @@ from bottle import default_app, get, post, template, request, static_file,\
 response
 import requests
 from requests_oauthlib import OAuth1, OAuth1Session
+
+scrobble = requests.get('http://ws.audioscrobbler.com/2.0/?')
+metodos = {}
+
+#hola
