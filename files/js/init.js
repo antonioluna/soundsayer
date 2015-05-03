7ac7032674bd4a28c14d80fbdd5a66c1de5a2ed6
Antonio Luna <antonio__luna@outlook.es>
2015-05-03 17:28:54 +0200

    cambiando plantilla

--- files/js/init.js
+++ files/js/init.js
@@ -1,62 +1,57 @@
 /*
-	Aerial by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	Iridium by TEMPLATED
+    templated.co @templatedco
+    Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 
-(function() {
-
-	skel.init({
-		reset: 'full',
-		breakpoints: {
-			'global': { range: '*', href: 'css/style.css', viewport: { scalable: false } },
-			'wide': { range: '-1680', href: 'css/style-wide.css' },
-			'normal': { range: '-1280', href: 'css/style-normal.css' },
-			'mobile': { range: '-736', href: 'css/style-mobile.css' },
-			'mobilep': { range: '-480', href: 'css/style-mobilep.css' }
+skel.init({
+	prefix: 'css/style',
+	resetCSS: true,
+	boxModel: 'border',
+	grid: {
+		gutters: 50
+	},
+	breakpoints: {
+		'mobile': {
+			range: '-480',
+			lockViewport: true,
+			containers: 'fluid',
+			grid: {
+				collapse: true,
+				gutters: 10
+			}
+		},
+		'desktop': {
+			range: '481-',
+			containers: 1200
+		},
+		'1000px': {
+			range: '481-1200',
+			containers: 960
 		}
-	});
-
-	// Events (JS).
-		
-		// Remove "loading" class once the page has fully loaded.
-			window.onload = function() {
-				document.body.className = '';
+	}
+}, {
+	panels: {
+		panels: {
+			navPanel: {
+				breakpoints: 'mobile',
+				position: 'left',
+				style: 'reveal',
+				size: '80%',
+				html: '<div data-action="navList" data-args="nav"></div>'
 			}
-
-		// Prevent scrolling on touch.
-			window.ontouchmove = function() {
-				return false;
+		},
+		overlays: {
+			titleBar: {
+				breakpoints: 'mobile',
+				position: 'top-left',
+				height: 44,
+				width: '100%',
+				html: '<span class="toggle" data-action="togglePanel" data-args="navPanel"></span>' +
+ '<span class="title" data-action="copyHTML" data-args="logo"></span>'
 			}
-
-		// Fix scroll position on orientation change.
-			window.onorientationchange = function() {
-				document.body.scrollTop = 0;
-			}
-
-	/*
-
-	// Events (jQuery).
-	// Aerial doesn't need jQuery, but if you're going to use it anyway remove the
-	// block of JS events above and use the jQuery-based ones below instead.
-
-		jQuery(window)
-
-			// Remove "loading" class once the page has fully loaded.
-				.on('load', function() {
-					jQuery('body').removeClass('loading');
-				})
-
-			// Prevent scrolling on touch.
-				.on('touchmove', function() {
-					return false;
-				})
-
-			// Fix scroll position on orientation change.
-				.on('orientationchange', function() {
-					jQuery('body').scrollTop(0);
-				});
-
-	*/
-
-})();
+		}
+	}
+	
+	
+});
